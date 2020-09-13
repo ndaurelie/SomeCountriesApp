ea55067d046ddec97c48128e7846c8fc6dabdfad
Aurélie Nouaille-Degorce <aurelie@colom.be>
2020-09-13 21:03:27 +0200

    Initial Commit

---
+++ SomeCountriesApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SomeCountriesApp
+//
+//  Created by Aurélie Nouaille-Degorce on 13/09/2020.
+//  Copyright © 2020 Aurélie Nouaille-Degorce. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
